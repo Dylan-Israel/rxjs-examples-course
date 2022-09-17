8248f8109c304d00e21324e91c54fc9e1e98cd83
D-Money Legend <dylansemail310@gmail.com>
2022-09-17 18:07:53 -0400

    Added initial of()

--- src/app/documentation/fundamentals/of-tap-pipe/of-tap-pipe.component.ts
+++ src/app/documentation/fundamentals/of-tap-pipe/of-tap-pipe.component.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
+import { of } from "rxjs";
 
 @Component({
     selector: 'app-of-tap-pipe',
     templateUrl: './of-tap-pipe.component.html',
     styleUrls: ['./of-tap-pipe.component.scss']
 })
-export class OfTapPipeComponent { }
+export class OfTapPipeComponent {
+    tapEx$ = of('Hello World');
+}
